bc6b39cc67c4226281e32c712b5c44ac16c3f8e4
Lev Maximov <lev.maximov@gmail.com>
2012-02-23 00:27:56 +0700

    store url as well

--- admin_honeypot/views.py
+++ admin_honeypot/views.py
@@ -10,17 +10,20 @@ from django.utils.translation import ugettext as _
 def admin_honeypot(request, extra_context=None):
     if not request.path.endswith('/'):
         return redirect(request.path + '/', permanent=True)
+    path = request.get_full_path()
 
     context = {
-        'app_path': request.get_full_path(),
+        'app_path': path,
         'form': HoneypotLoginForm(request, request.POST or None),
-        REDIRECT_FIELD_NAME: request.get_full_path(),
+        REDIRECT_FIELD_NAME: path,
         'site': Site.objects.get_current(),
         'title': _('Log in'),
         'fake_admin': True,
         }
     context['form'].is_valid()
     context.update(extra_context or {})
+    if len(path)>255:
+        path = path[:230] +  '...(%d chars)' % len(path)
     if request.method == 'POST':
         failed = LoginAttempt.objects.create(
             username=request.POST.get('username'),
@@ -28,6 +31,7 @@ def admin_honeypot(request, extra_context=None):
             session_key=request.session.session_key,
             ip_address=request.META.get('REMOTE_ADDR'),
             user_agent=request.META.get('HTTP_USER_AGENT'),
+            path=path,
             )
         honeypot.send(sender=LoginAttempt, instance=failed, request=request)
     return render_to_response('admin/login.html', context, context_instance=RequestContext(request))
